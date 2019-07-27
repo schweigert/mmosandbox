acfc5ac1b49371b09104f0d0996a605d8ff870df
schweigert <marlon.henry@magrathealabs.com>
2019-07-27 13:07:15 -0300

    Account controller test

---
+++ infra/controllers/account_controller_test.go
@@ -0,0 +1,68 @@
+package controllers
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/schweigert/mmosandbox/infra/db"
+
+	"github.com/gin-gonic/gin"
+	"github.com/schweigert/mmosandbox/domain"
+	"github.com/schweigert/mmosandbox/infra/dbrepositories"
+	"github.com/schweigert/mmosandbox/infra/routes"
+
+	"github.com/stretchr/testify/suite"
+)
+
+type AccountControllerSuite struct {
+	suite.Suite
+}
+
+func (suite *AccountControllerSuite) SetupTest() {
+	domain.AccountRepository = dbrepositories.NewAccountRepository()
+}
+
+func (suite *AccountControllerSuite) TestNewAccountController() {
+	suite.NotNil(NewAccountController())
+}
+
+func (suite *AccountControllerSuite) TestRoutes() {
+	engine := gin.Default()
+	NewAccountController().Routes(engine)
+
+	suite.Equal(1, len(engine.Routes()))
+}
+
+func (suite *AccountControllerSuite) TestCreate() {
+	db.Clear()
+	db.Migrate()
+
+	input := domain.NewCreateAccountInput()
+
+	input.Email = "testing@example.onion"
+	input.Username = "a_very_secure_user"
+	input.Password = "a very secure pass"
+
+	inputBody, err := json.Marshal(input)
+
+	suite.NoError(err)
+
+	req, err := http.NewRequest(http.MethodPut, routes.Account, bytes.NewReader(inputBody))
+	req.Header.Set("Content-Type", "application/json")
+
+	suite.NoError(err)
+
+	rec := httptest.NewRecorder()
+	engine := gin.Default()
+	NewAccountController().Routes(engine)
+	engine.ServeHTTP(rec, req)
+
+	suite.Equal(http.StatusCreated, rec.Code)
+}
+
+func TestAccountControllerSuite(t *testing.T) {
+	suite.Run(t, new(AccountControllerSuite))
+}
