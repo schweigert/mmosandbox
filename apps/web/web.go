1ceddb92a4baa9845f6966c7c4e04dfeeb7dad8d
schweigert <marlon.henry@magrathealabs.com>
2019-07-26 22:54:56 -0300

    Web server is upping :tada:

--- apps/web/web.go
+++ apps/web/web.go
@@ -2,10 +2,18 @@ package main
 
 import (
 	"github.com/schweigert/mmosandbox/config"
+	"github.com/schweigert/mmosandbox/infra/controllers"
 	"github.com/schweigert/mmosandbox/lib/dont"
 	"github.com/schweigert/mmosandbox/lib/web"
 )
 
+var app *web.App
+var usedControllers = []web.Controller{
+	controllers.NewRootController(),
+}
+
 func main() {
-	dont.Panic(web.NewApp(config.Web().Addr()).Run())
+	app = web.NewApp(config.Web().Addr())
+	app.Setup(usedControllers...)
+	dont.Panic(app.Run())
 }
