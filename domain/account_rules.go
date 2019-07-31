226ebfe1b469ed6b69c8f1e67ce77f823c3b8148
schweigert <marlon.henry@magrathealabs.com>
2019-07-30 23:55:10 -0300

    Account rules

--- domain/account_rules.go
+++ domain/account_rules.go
@@ -1,9 +1,19 @@
 package domain
 
+import (
+	"github.com/schweigert/mmosandbox/domain/inputs"
+	"github.com/schweigert/mmosandbox/domain/outputs"
+)
+
 // AccountRules operations over this account
 type AccountRules struct{}
 
 // NewAccountRules constructor
 func NewAccountRules() *AccountRules {
 	return &AccountRules{}
+}
+
+// CreateCharacter based in this input
+func (rules *AccountRules) CreateCharacter(in *inputs.CreateCharacterInput) *outputs.CreateCharacterOutput {
+	return nil
 }
