2a262c95afdd3e0445b784ea7bd6587a05566337
schweigert <marlon.henry@magrathealabs.com>
2019-08-03 14:03:26 -0300

    Use TokenRepository over redis

--- apps/wauth/wauth.go
+++ apps/wauth/wauth.go
@@ -5,15 +5,35 @@ import (
 	"net/rpc"
 
 	"github.com/schweigert/mmosandbox/config"
+	"github.com/schweigert/mmosandbox/infra/db"
+	"github.com/schweigert/mmosandbox/infra/dbrepositories"
+	"github.com/schweigert/mmosandbox/infra/merepositories"
 	"github.com/schweigert/mmosandbox/infra/tasks"
 	"github.com/schweigert/mmosandbox/lib/dont"
 )
 
-func main() {
+func configCache() {
+	merepositories.UseTokenRepository()
+}
+
+func configDb() {
+	dbrepositories.UseAccountRepository()
+	dbrepositories.UseCharacterRepository()
+
+	db.Migrate()
+}
+
+func startRPC() {
 	listener, err := net.Listen("tcp", config.RPC().Addr())
 	dont.Panic(err)
 
 	dont.Panic(rpc.Register(tasks.NewSessionTask()))
 
 	rpc.Accept(listener)
+}
+
+func main() {
+	configCache()
+	configDb()
+	startRPC()
 }
