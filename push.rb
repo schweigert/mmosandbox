ee0b800ec13a2456459ed7bc0bc7276cdad05e84
schweigert <marlon.henry@magrathealabs.com>
2019-08-04 14:06:19 -0300

    Push to dockerhub

---
+++ push.rb
@@ -0,0 +1,32 @@
+# docker tag local-image:tagname new-repo:tagname
+# docker push new-repo:tagname
+
+def remove_prefix(service)
+    service.gsub('mmosandbox_', '')
+end
+
+def now
+    @now = Time.now
+end
+
+def lasted(service)
+    [remove_prefix(service), 'lasted'].join('.')
+end
+
+def dated(service)
+    [remove_prefix(service), now.year, now.month, now.day, now.hour, now.hour, now.min].join('.')
+end
+
+services = %w[
+    mmosandbox_wclient
+    mmosandbox_wweb
+    mmosandbox_wgame
+    mmosandbox_wauth
+]
+
+services.each do |service|
+    [dated(service), lasted(service)].each do |tag|
+        system("docker tag #{service} schweigert/mmosandbox:#{tag}")
+        system("docker push schweigert/mmosandbox:#{tag}")
+    end
+end
