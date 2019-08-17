301eadb3fca19d0d43f06dcf33520ef63371dc72
schweigert <marlon.henry@magrathealabs.com>
2019-08-17 17:52:59 -0300

    Listen chat

--- infra/tasks/game_task.go
+++ infra/tasks/game_task.go
@@ -76,3 +76,19 @@ func (task *GameTask) SendChat(in inputs.ChatInput, out *outputs.CheckSessionOut
 		return err
 	})
 }
+
+// ListenChat task
+func (task *GameTask) ListenChat(in inputs.ChatInput, out *outputs.ListenMessagesOutput) error {
+	return bench.Bench("listen_chat", func() (err error) {
+		checkOut := outputs.NewCheckSessionOutput()
+		err = task.SessionConn.Call("SessionTask.CheckSession", in.CheckSessionInput, checkOut)
+
+		out.Success = checkOut.Success
+
+		if err == nil && out.Success {
+			err = task.WorldRules.CharacterListen(&in, out)
+		}
+
+		return err
+	})
+}
