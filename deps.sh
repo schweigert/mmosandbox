18dcb45168a9378c3d757a6ad95eee7375aed3f1
schweigert <marlon.henry@magrathealabs.com>
2019-08-18 13:23:16 -0300

    Recolor deps =D

--- deps.sh
+++ deps.sh
@@ -1,7 +1,7 @@
-godepgraph -nostdlib -horizontal github.com/schweigert/mmosandbox/domain/ | dot -Tpng -o domain/deps.png
+godepgraph -nostdlib -horizontal github.com/schweigert/mmosandbox/domain/ | ruby deps_recolor.rb | dot -Tpng -o domain/deps.png
 
-godepgraph -nostdlib -horizontal github.com/schweigert/mmosandbox/apps/wweb | dot -Tpng -o apps/wweb/deps.png
-godepgraph -nostdlib -horizontal github.com/schweigert/mmosandbox/apps/wauth | dot -Tpng -o apps/wauth/deps.png
-godepgraph -nostdlib -horizontal github.com/schweigert/mmosandbox/apps/wgame | dot -Tpng -o apps/wgame/deps.png
+godepgraph -nostdlib -horizontal github.com/schweigert/mmosandbox/apps/wweb | ruby deps_recolor.rb | dot -Tpng -o apps/wweb/deps.png
+godepgraph -nostdlib -horizontal github.com/schweigert/mmosandbox/apps/wauth | ruby deps_recolor.rb | dot -Tpng -o apps/wauth/deps.png
+godepgraph -nostdlib -horizontal github.com/schweigert/mmosandbox/apps/wgame | ruby deps_recolor.rb | dot -Tpng -o apps/wgame/deps.png
 
-godepgraph -nostdlib -horizontal github.com/schweigert/mmosandbox/clients/wclient | dot -Tpng -o clients/wclient/deps.png
+godepgraph -nostdlib -horizontal github.com/schweigert/mmosandbox/clients/wclient | ruby deps_recolor.rb | dot -Tpng -o clients/wclient/deps.png
---
+++ deps_recolor.rb
@@ -0,0 +1,47 @@
+#!/usr/bin/env ruby
+# https://www.graphviz.org/doc/info/colors.html
+
+def is_domain?(line)
+    line.include?('mmosandbox/domain')
+end
+
+def recolor_domain(line)
+    line.gsub('paleturquoise', 'coral')
+end
+
+def is_clients?(line)
+    line.include?('mmosandbox/clients')
+end
+
+def recolor_clients(line)
+    line.gsub('paleturquoise', 'gold')
+end
+
+def is_lib?(line)
+    line.include?('mmosandbox/lib')
+end
+
+def recolor_lib(line)
+    line.gsub('paleturquoise', 'azure3')
+end
+
+def is_infra?(line)
+    line.include?('mmosandbox/infra')
+end
+
+def recolor_infra(line)
+    line.gsub('paleturquoise', 'springgreen')
+end
+
+def sub(line)
+    return recolor_domain(line) if is_domain?(line)
+    return recolor_lib(line) if is_lib?(line)
+    return recolor_clients(line) if is_clients?(line)
+    return recolor_infra(line) if is_infra?(line)
+
+    return line
+end
+
+STDIN.read.split("\n").each do |line|
+    puts sub(line)
+ end
