18a5dadf773ad2c77de9b45f59a13c45b6db64f2
schweigert <marlon.henry@magrathealabs.com>
2019-09-07 11:17:48 -0300

    Update swarm to teatcher pc

--- swarm-join.sh
+++ swarm-join.sh
@@ -1,1 +1,1 @@
-sudo docker swarm join --token SWMTKN-1-361bsbdwodf23mjmqxl7gir14bg5bhrs3r6yi4s0he80suvj40-b7fj9lu0l5b5qw02h1mjttp2z 192.168.4.241:2377
+docker swarm join --token SWMTKN-1-4rznfv7m66j4kqd05a0ggzj97q1axbqjyjgimkgksai6hsslsi-bp4w27aktffu8ceckfy9m6qq1 192.168.7.236:2377
