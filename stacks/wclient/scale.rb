7359c3655298de7e41acf44cda5b3deddd7016d6
schweigert <marlon.henry@magrathealabs.com>
2019-09-14 12:15:55 -0300

    update scale

--- stacks/wclient/scale.rb
+++ stacks/wclient/scale.rb
@@ -1,4 +1,10 @@
-25.times do |n|
-    sleep 60
-    system("sudo docker service scale wclient_wclient=#{(n + 1) * 2}")
+system("sudo docker stack deploy --compose-file docker-compose.yml wclient")
+system("sudo docker service scale wclient_wclient=0")
+sleep 5 * 60
+system("sudo docker service scale wclient_wclient=1")
+sleep 5 * 60
+
+100.times do |n|
+    sleep 30
+    system("sudo docker service scale wclient_wclient=#{1 + n}")
 end
