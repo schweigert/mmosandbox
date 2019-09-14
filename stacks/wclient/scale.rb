fbf56621532f893ae360993da255387e836065a5
schweigert <marlon.henry@magrathealabs.com>
2019-09-14 12:20:22 -0300

    Stop scale

--- stacks/wclient/scale.rb
+++ stacks/wclient/scale.rb
@@ -1,8 +1,4 @@
 system("sudo docker stack deploy --compose-file docker-compose.yml wclient")
-system("sudo docker service scale wclient_wclient=0")
-sleep 5 * 60
-system("sudo docker service scale wclient_wclient=1")
-sleep 5 * 60
 
 100.times do |n|
     sleep 30
